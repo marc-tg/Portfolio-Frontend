c43b47a41459970f994fbbe89a3b0e80384f7251
marc-tg <marctg2005@gmail.com>
2025-08-02 16:43:03 +0200

    Experience (no responsive)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { ExperienceComponent } from './components/experience/experience.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, ExperienceComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
