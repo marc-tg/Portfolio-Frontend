e74b6db25b7e21c3eac907ac148f6fac10c5df06
marc-tg <marctg2005@gmail.com>
2025-08-02 17:00:53 +0200

    Created technologies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { ExperienceComponent } from './components/experience/experience.component';
+import { TechnologiesComponent } from './components/technologies/technologies.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, ExperienceComponent],
+  imports: [RouterOutlet, HeaderComponent, ExperienceComponent, TechnologiesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
